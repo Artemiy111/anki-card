34f84b4ed3892b70fa134dd28a3656806a4cc3d9
Artemiy111 <mrart111@mail.ru>
2025-03-31 13:53:51 +0300

    .

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -8,22 +8,27 @@
   >
     <section class="flex w-full flex-col gap-y-4">
       <div class="relative">
-        <h1 class="mx-8 text-center text-3xl font-bold">{@html card.expression}</h1>
-        <label class="absolute top-2 right-0 cursor-pointer text-violet-500">
-          <svg
-            xmlns="http://www.w3.org/2000/svg"
-            width="24"
-            height="24"
-            viewBox="0 0 24 24"
-            fill="none"
-            stroke="currentColor"
-            stroke-width="2"
-            stroke-linecap="round"
-            stroke-linejoin="round"
-            class="lucide lucide-play-icon lucide-play"><polygon points="6 3 20 12 6 21 6 3" /></svg
-          >
-          <div class="absolute invisible">{@html card['expression-audio']}</div>
-        </label>
+        {#if card['expression-audio']}
+          <h1 class="mx-8 text-center text-3xl font-bold">{@html card.expression}</h1>
+          <label class="absolute top-2 right-0 cursor-pointer text-violet-500">
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              width="24"
+              height="24"
+              viewBox="0 0 24 24"
+              fill="none"
+              stroke="currentColor"
+              stroke-width="2"
+              stroke-linecap="round"
+              stroke-linejoin="round"
+              class="lucide lucide-play-icon lucide-play"
+              ><polygon points="6 3 20 12 6 21 6 3" /></svg
+            >
+            <div class="hidden">{@html card['expression-audio']}</div>
+          </label>
+        {:else}
+        <h1 class="text-center text-3xl font-bold">{@html card.expression}</h1>
+        {/if}
       </div>
       <div class="text-xl text-slate-600 dark:text-slate-200">
         <span>{@html card['cloze-prefix']}</span>
--- src/routes/back/+page.svelte
+++ src/routes/back/+page.svelte
@@ -2,32 +2,35 @@
   import { card } from '$lib/card'
 </script>
 
-<main class="mx-auto min-h-dvh w-fit my-10 dark:bg-slate-800">
+<main class="mx-auto my-10 min-h-dvh w-fit dark:bg-slate-800">
   <div
     class="flex min-h-60 w-lg flex-col items-center gap-6 rounded-4xl bg-slate-50 p-12 pt-10 text-xl dark:bg-slate-950 dark:text-white"
   >
     <section class="flex w-full flex-col gap-y-4">
       <h1 class="text-center text-3xl font-bold">{@html card.expression}</h1>
 
-      <div class="grid grid-cols-[1fr_min-content] items-baseline gap-4">
+      <div class="grid grid-cols-[1fr_min-content]">
         <div class="dark:text-slate-200">
           {@html card.sentence}
         </div>
-        <label class="relative cursor-pointer text-violet-500">
-          <svg
-            xmlns="http://www.w3.org/2000/svg"
-            width="24"
-            height="24"
-            viewBox="0 0 24 24"
-            fill="none"
-            stroke="currentColor"
-            stroke-width="2"
-            stroke-linecap="round"
-            stroke-linejoin="round"
-            class="lucide lucide-play-icon lucide-play"><polygon points="6 3 20 12 6 21 6 3" /></svg
-          >
-          <div class="absolute invisible">{@html card['sentence-audio']}</div>
-        </label>
+        {#if card['sentence-audio']}
+          <label class="relative ml-4 cursor-pointer text-violet-500">
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              width="24"
+              height="24"
+              viewBox="0 0 24 24"
+              fill="none"
+              stroke="currentColor"
+              stroke-width="2"
+              stroke-linecap="round"
+              stroke-linejoin="round"
+              class="lucide lucide-play-icon lucide-play"
+              ><polygon points="6 3 20 12 6 21 6 3" /></svg
+            >
+            <div class="hidden">{@html card['sentence-audio']}</div>
+          </label>
+        {/if}
       </div>
       <label for="translation" class=" text-slate-600 dark:text-slate-500">
         <input type="checkbox" name="" id="translation" class="peer hidden appearance-none" />
@@ -38,17 +41,25 @@
         </div>
       </label>
     </section>
-    <div class="[&>img]:w-full [&>img]:rounded-3xl">
-      {@html card.image}
-    </div>
-    <section class="flex flex-col gap-y-4">
-      <div class="grid grid-cols-[min-content_1fr] gap-4 text-slate-800 dark:text-slate-200">
-        <div class="h-full w-1 bg-violet-500"></div>
-        {@html card.notes}
+
+    {#if card.image}
+      <div class="[&>img]:w-full [&>img]:rounded-3xl">
+        {@html card.image}
       </div>
-      <div class="text-lg w-full p-0">
+    {/if}
+
+    <section class="flex flex-col gap-y-4">
+      {#if card.notes}
+        <div class="grid grid-cols-[min-content_1fr] gap-4 text-slate-800 dark:text-slate-200">
+          <div class="h-full w-1 bg-violet-500"></div>
+          {@html card.notes}
+        </div>
+      {/if}
+
+      <div class="w-full p-0 text-lg">
         {@html card.meaning}
       </div>
+      
       <div class="">
         <div class="relative w-fit text-violet-500">
           <svg
@@ -67,7 +78,7 @@
             /></svg
           >
           <span
-            class="absolute inset-0 z-1 w-full text-transparent overflow-hidden whitespace-nowrap" 
+            class="absolute inset-0 z-1 w-full overflow-hidden whitespace-nowrap text-slate-50"
           >
             {@html card.url}
           </span>
