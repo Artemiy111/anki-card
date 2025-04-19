62e45306137731a3b3fc7c0268b66675998dbac8
Artemiy111 <mrart111@mail.ru>
2025-04-19 18:10:30 +0300

    .

--- src/app/routes/+layout.svelte
+++ src/app/routes/+layout.svelte
@@ -2,7 +2,7 @@
   import ReverseButton from '~/shared/ui/reverse-button.svelte'
   import CodeDialogButton from '~/shared/ui/code-dialog-button.svelte'
   import DarkmodeButton from '~/shared/ui/darkmode-button.svelte'
-  import {PenIcon} from 'lucide-svelte'
+  import { PenIcon } from 'lucide-svelte'
 
   import '~/app/app.css'
   let { data, children } = $props()
@@ -18,7 +18,10 @@
   <div
     class="fixed right-[calc(8*var(--spacing)+100%-100dvw)] bottom-4 flex flex-col gap-4 lg:right-[calc(50%-var(--container-xl)/2-24px-50px)]"
   >
-    <PenIcon class="text-text cursor-pointer" onclick={() => fetch('/api/anki', {method: 'POST'})} />
+    <PenIcon
+      class="text-text cursor-pointer"
+      onclick={() => fetch('/api/anki', { method: 'POST' })}
+    />
     <DarkmodeButton bind:isDark={data.isDark} />
     <ReverseButton />
     <CodeDialogButton code={data.code} />
--- src/app/routes/api/anki/+server.ts
+++ src/app/routes/api/anki/+server.ts
@@ -3,7 +3,6 @@ import { YankiConnect } from 'yanki-connect'
 
 const yanki = new YankiConnect()
 
-
 export const POST = async ({ request }) => {
   const data = await yanki.deck.deckNames()
   console.log(data)
