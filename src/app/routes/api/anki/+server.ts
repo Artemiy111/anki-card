2122bfcc51a23c79bf476bd66160e3a04f484acd
Artemiy111 <mrart111@mail.ru>
2025-04-19 18:10:22 +0300

    .

---
+++ src/app/routes/api/anki/+server.ts
@@ -0,0 +1,11 @@
+import { json } from '@sveltejs/kit'
+import { YankiConnect } from 'yanki-connect'
+
+const yanki = new YankiConnect()
+
+
+export const POST = async ({ request }) => {
+  const data = await yanki.deck.deckNames()
+  console.log(data)
+  return json(data)
+}
